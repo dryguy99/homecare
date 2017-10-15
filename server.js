d2bf35ccfbc3552188b8bc0e68a0dba030444088
Timothy Dry <timothy.dry@verizon.net>
2017-10-15 00:53:25 -0400

    disabled mongo

--- server.js
+++ server.js
@@ -39,17 +39,17 @@ request(queryURL02, function (error, response, body) {
   console.log('body:', body); // Print the HTML for the Google homepage.
 });
 
-var configDB = require('./config/database.js');
-mongoose.Promise = Promise;
-
-
-if (!process.env.MONGODB_URI){
-
-        mongoose.connect("mongodb://localhost/img");
-    }
-    else{
-        mongoose.connect(process.env.MONGODB_URI)
-    }
+// var configDB = require('./config/database.js');
+// mongoose.Promise = Promise;
+//
+//
+// if (!process.env.MONGODB_URI){
+//
+//         mongoose.connect("mongodb://localhost/img");
+//     }
+//     else{
+//         mongoose.connect(process.env.MONGODB_URI)
+//     }
 require('./config/passport')(passport); // pass passport for configuration
 //app.use(morgan('dev')); // log every request to the console
 //app.use(express.bodyParser({uploadDir:'./uploads'}));
@@ -65,13 +65,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 var db= mongoose.connection;
-app.use(flash()); // use connect-flash for flash messages stored in session
-  db.on("error", function(error){
-  console.log("Mongoose Error",error)
-  });
-  db.once("open", function(error){
-  console.log("Mongoose Rocks")
-  });
+// app.use(flash()); // use connect-flash for flash messages stored in session
+//   db.on("error", function(error){
+//   console.log("Mongoose Error",error)
+//   });
+//   db.once("open", function(error){
+//   console.log("Mongoose Rocks")
+//   });
 // routes ======================================================================
 require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 require('./index.js');
